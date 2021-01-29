50516c2d74d19477b270d325defe414d23de0189
jacknq <bousekr@gmail.com>
2021-01-29 14:10:43 +0100

    refact

--- src/vsextention/texttemplating/dist/extension.js
+++ src/vsextention/texttemplating/dist/extension.js
@@ -14,23 +14,75 @@ const vscode = __webpack_require__(1);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+    let NEXT_TERM_ID = 1;
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "texttemplating" is now active!');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('tt.helloWorld', () => {
+    let disposable = vscode.commands.registerCommand('tt.helloWorlds', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World from texttemplating!');
     });
+    context.subscriptions.push(vscode.commands.registerCommand('tt.helloWorld', (uri) => {
+        const terminal = vscode.window.createTerminal(`TT #${NEXT_TERM_ID++}`);
+        terminal.sendText(`echo 'Sent text immediately after creating ${uri.fsPath}'`);
+        terminal.show();
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('terminalTest.show', () => {
+        if (ensureTerminalExists()) {
+            selectTerminal().then(terminal => {
+                if (terminal) {
+                    terminal.show();
+                }
+            });
+        }
+    }));
+    //https://github.com/microsoft/vscode-extension-samples/blob/master/terminal-sample/src/extension.ts
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
+function colorText(text) {
+    let output = '';
+    let colorIndex = 1;
+    for (let i = 0; i < text.length; i++) {
+        const char = text.charAt(i);
+        if (char === ' ' || char === '\r' || char === '\n') {
+            output += char;
+        }
+        else {
+            output += `\x1b[3${colorIndex++}m${text.charAt(i)}\x1b[0m`;
+            if (colorIndex > 6) {
+                colorIndex = 1;
+            }
+        }
+    }
+    return output;
+}
+function selectTerminal() {
+    const terminals = vscode.window.terminals;
+    const items = terminals.map(t => {
+        return {
+            label: `name: ${t.name}`,
+            terminal: t
+        };
+    });
+    return vscode.window.showQuickPick(items).then(item => {
+        return item ? item.terminal : undefined;
+    });
+}
+function ensureTerminalExists() {
+    if (vscode.window.terminals.length === 0) {
+        vscode.window.showErrorMessage('No active terminals');
+        return false;
+    }
+    return true;
+}
 
 
 /***/ }),
--- src/vsextention/texttemplating/src/extension.ts
+++ src/vsextention/texttemplating/src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
+	let NEXT_TERM_ID = 1;
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "texttemplating" is now active!');
@@ -13,15 +13,70 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tt.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('tt.helloWorlds', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from texttemplating!');
+		
 	});
-
+	context.subscriptions.push(vscode.commands.registerCommand('tt.helloWorld',  (uri:vscode.Uri)  => {
+		const terminal = vscode.window.createTerminal(`TT #${NEXT_TERM_ID++}`);
+		terminal.sendText(`echo 'Sent text immediately after creating ${uri.fsPath}'`);
+		terminal.show();
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('terminalTest.show', () => {
+		if (ensureTerminalExists()) {
+			selectTerminal().then(terminal => {
+				if (terminal) {
+					terminal.show();
+				}
+			});
+		}
+	}));
+ //https://github.com/microsoft/vscode-extension-samples/blob/master/terminal-sample/src/extension.ts
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+function colorText(text: string): string {
+	let output = '';
+	let colorIndex = 1;
+	for (let i = 0; i < text.length; i++) {
+		const char = text.charAt(i);
+		if (char === ' ' || char === '\r' || char === '\n') {
+			output += char;
+		} else {
+			output += `\x1b[3${colorIndex++}m${text.charAt(i)}\x1b[0m`;
+			if (colorIndex > 6) {
+				colorIndex = 1;
+			}
+		}
+	}
+	return output;
+}
+
+function selectTerminal(): Thenable<vscode.Terminal | undefined> {
+	interface TerminalQuickPickItem extends vscode.QuickPickItem {
+		terminal: vscode.Terminal;
+	}
+	const terminals = <vscode.Terminal[]>(<any>vscode.window).terminals;
+	const items: TerminalQuickPickItem[] = terminals.map(t => {
+		return {
+			label: `name: ${t.name}`,
+			terminal: t
+		};
+	});
+	return vscode.window.showQuickPick(items).then(item => {
+		return item ? item.terminal : undefined;
+	});
+}
+
+function ensureTerminalExists(): boolean {
+	if ((<any>vscode.window).terminals.length === 0) {
+		vscode.window.showErrorMessage('No active terminals');
+		return false;
+	}
+	return true;
+}
